6ee60df72bc074d5c2744bcd8d13b8cf8f2ba7cf
Jaikt Dev <jaikt.dev@gmail.com>
2021-12-27 15:47:07 +0530

    testing bid

---
+++ dist/config.example.php
@@ -0,0 +1,13 @@
+<?php
+
+// SMTP Hostname (eg. smtp.gmail.com)
+define('smtp_hostname', 'smtp.gmail.com');
+// SMTP Username (eg. jaikt.dev@gmail.com)
+define('smtp_username', 'jaikt.dev@gmail.com');
+// SMTP Password (eg. 2fb1ba86637a434c3288b0c2eafd6e3d)
+define('smtp_password', '2fb1ba86637a434c3288b0c2eafd6e3d');
+// Email Of Reciever (eg. support@royalityfree.com)
+define('smtp_recform', 'support@royalityfree.com');
+
+
+?>
